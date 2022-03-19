6999975c9a103c64e7c09617bc6b19aff96b32e4
avc0021 <avc0021@gmail.com>
2022-03-19 11:19:41 -0500

    navbar/header redo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,67 +1,88 @@
 import React from 'react';
-import { capitalizeFirstLetter } from '../../utils/helpers';
+import Navbar from '../Navbar'
+//import { capitalizeFirstLetter } from '../../utils/helpers';
 
-function Header(props) {
+function Header () {
 
-    const {
-        categories = [],
-        setCurrentCategory,
-        contactSelected,
-        currentCategory,
-        setContactSelected,
-      } = props;
-  
     return (
-      <header className="flex-row px-1">
-        <h2>
-          <a data-testid="link" href="/">
-             Adam Castro
-          </a>
-        </h2>
-        <nav>
-          <ul className="flex-row">
-            <li className="mx-2">
-              <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}>
-                About me
-              </a>
-            </li>
-            <li className={`mx-2 ${contactSelected && 'navActive'}`}>
-              <span onClick={() => setContactSelected(true)}>Contact</span>
-            </li>
-            {categories.map((category) => (
-              <li
-                className={`mx-1 ${
-                  currentCategory.name === category.name && !contactSelected && 'navActive'
-                  }`}
-                key={category.name}
-              >
-                <span
-                  onClick={() => {
-                    setCurrentCategory(category);
-                    setContactSelected(false);
-                  }}
-                >
-                  {capitalizeFirstLetter(category.name)}
-                </span>
-              </li>
-            ))}
-          </ul>
-        </nav>
-      </header>
-    );
+      <section className="header">
+        <section className="header-top">
+          <section className="header-top__logo">
+            <a href="/" className="header-logo">Adam Castro</a>
+          </section>
+          <section className="header-top__navbar">
+            <section className="header-top__navigation">
+              <Navbar />
+            </section>
+            <hr className="header-top__seperator" />
+          </section>
+        </section>
+      </section>
+    )
   }
+  
+  export default Header;
+// function Header(props) {
 
-  export default Header({
-    Home: {
-      screen: HomeScreen
-    },
-  }, {
-    navigationOptions: {
-      headerBackground: () => (
-        <Image
-          style={StyleSheet.absoluteFill}
-          source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/3/36/Hopetoun_falls.jpg' }}
-        />
-      ),
-    }
-  });
+//     const {
+//         categories = [],
+//         setCurrentCategory,
+//         contactSelected,
+//         currentCategory,
+//         setContactSelected,
+//       } = props;
+  
+//     return (
+//       <header className="flex-row px-1">
+//         <h2>
+//           <a data-testid="link" href="/">
+//              Adam Castro
+//           </a>
+//         </h2>
+//         <nav>
+//           <ul className="flex-row">
+//             <li className="mx-2">
+//               <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}>
+//                 About me
+//               </a>
+//             </li>
+//             <li className={`mx-2 ${contactSelected && 'navActive'}`}>
+//               <span onClick={() => setContactSelected(true)}>Contact</span>
+//             </li>
+//             {categories.map((category) => (
+//               <li
+//                 className={`mx-1 ${
+//                   currentCategory.name === category.name && !contactSelected && 'navActive'
+//                   }`}
+//                 key={category.name}
+//               >
+//                 <span
+//                   onClick={() => {
+//                     setCurrentCategory(category);
+//                     setContactSelected(false);
+//                   }}
+//                 >
+//                   {capitalizeFirstLetter(category.name)}
+//                 </span>
+//               </li>
+//             ))}
+//           </ul>
+//         </nav>
+//       </header>
+//     );
+//   }
+
+//   export default Header({
+//     Home: {
+//       screen: HomeScreen
+//     },
+//   }, {
+//     navigationOptions: {
+//       headerBackground: () => (
+//         <Image
+//           style={StyleSheet.absoluteFill}
+//           source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/3/36/Hopetoun_falls.jpg' }}
+//         />
+//       ),
+//     }
+//   });
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Navbar () {
+
+    return (
+      <section className="navbar">
+        <a href="/" className="navbar-item">Home</a>
+        <a href="/about" className="navbar-item">About</a>
+        <a href="/projects" className="navbar-item">Portfolio</a>
+        <a href="/contact" className="navbar-item">Contact</a>
+    </section>
+    )
+  
+  }
+  
+  export default Navbar;
