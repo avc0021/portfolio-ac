c92977d37d01ea52121f790c858f98ce70e43715
avc0021 <avc0021@gmail.com>
2022-03-20 17:17:48 -0500

    correct buttons present and links working. size issue fixed. alignment issue in progress

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,20 +1,32 @@
-import React from 'react';
+import React from "react";
 
 function Footer() {
-    return (
-        <section className='footer'>
-            <div class="footer-social-icons">
-    
-    <ul class="social-icons">
-        <li><a href="" class="social-icon"> <i class="fa fa-facebook"></i></a></li>
-        <li><a href="" class="social-icon"> <i class="fa fa-twitter"></i></a></li>
-        <li><a href="" class="social-icon"> <i class="fa fa-youtube"></i></a></li>
-        <li><a href="" class="social-icon"> <i class="fa fa-linkedin"></i></a></li>
-        
-    </ul>
-</div>
-        </section>
-    );
+  return (
+    <section className="footer">
+      <div class="footer-social-icons">
+        <ul class="social-icons">
+          <li>
+            <a href="https://github.com/avc0021" class="social-icon">
+              {" "}
+              <i class="fa fa-github"></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/ac383/" class="social-icon">
+              {" "}
+              <i class="fa fa-linkedin"></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.instagram.com/_ayejayz_/" class="social-icon">
+              {" "}
+              <i class="fa fa-instagram"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
 }
 
 export default Footer;
