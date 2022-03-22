db81ee862ebb4062e42606be5ab57c8180ac7fe5
avc0021 <avc0021@gmail.com>
2022-03-21 20:29:23 -0500

    portfolio, resume, contact all have containers added. still working on alignment issues

--- src/components/Project3/index.js
+++ src/components/Project3/index.js
@@ -9,7 +9,7 @@ function Project3 () {
           <div className="bt">
             <h3 class="bt-1">Budget Tracker</h3>
             <p>An app that tells you what is in the stars above you based on your geolocation or city input.</p>
-            <a href="https://github.com/coevpen/my-night-sky">View Project on GitHub</a>
+            <a href="https://github.com/avc0021/Budget-Tracker">View Project on GitHub</a>
           </div>
       </section>
     )
