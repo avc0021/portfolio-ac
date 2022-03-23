14da6f704e30df5b2b948deb36373ad229138c21
avc0021 <91796423+avc0021@users.noreply.github.com>
2022-03-23 15:47:27 -0500

    Update index.js

--- src/components/Project1/index.js
+++ src/components/Project1/index.js
@@ -10,10 +10,10 @@ function Project1 () {
           <div className="mns">
             <h3 className="mns-1">My Night Sky</h3>
             <img src={mns} className="pImg" style={{ width: "30%" }} alt="cover" />
-            <p className="descr">An app that tells you what is in the stars above you based on your geolocation or city input.</p>
+            <p className="descr">This app that tells you what is in the stars above you based on your geolocation or city input.</p>
             <h3>
             <a href="https://github.com/coevpen/my-night-sky" >View Project on GitHub</a> <br></br>
-              <a href="https://github.com/coevpen/my-night-sky/deployments/activity_log?environment=github-pages">
+              <a href="https://coevpen.github.io/my-night-sky/">
                 Deployed Link
               </a>
             </h3>
