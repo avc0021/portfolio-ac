d9558c9d752b24776421c3e499a0a856d050a35b
avc0021 <avc0021@gmail.com>
2022-03-22 23:23:01 -0500

    fixed alignment issues with containers in projects

--- src/components/Project3/index.js
+++ src/components/Project3/index.js
@@ -4,11 +4,11 @@ import bt from '../../assets/proj-img/budget.png';
 function Project3 () {
 
     return (
-      <section className="projects">
+      <section className="projects-bt">
 
           <div className="bt">
             <h3>Budget Tracker</h3>
-            <img src={bt} className="pImg" style={{ width: "50%" }} alt="cover" />
+            <img src={bt} className="pImg" style={{ width: "65%" }} alt="cover" />
             <p className="descr">An app that tells you what is in the stars above you based on your geolocation or city input.</p>
             <h3>
             <a href="https://github.com/avc0021/Budget-Tracker">View Project on GitHub</a><br></br>
--- src/components/Project4/index.js
+++ src/components/Project4/index.js
@@ -5,11 +5,11 @@ import iru from '../../assets/proj-img/iru.png';
 function Project4 () {
 
     return (
-      <section className="projects">
+      <section className="projects-iru">
 
           <div className="iru">
             <h3 className="iru-1">Instruments R Us</h3>
-            <img src={iru} className="pImg" style={{ width: "50%" }} alt="cover" />
+            <img src={iru} className="pImg" style={{ width: "80%" }} alt="cover" />
             <p className="descr">An app that tells you what is in the stars above you based on your geolocation or city input.</p>
             <h3>
             <a href="https://github.com/avc0021/InstrumentsRUs">View Project on GitHub</a><br></br>
