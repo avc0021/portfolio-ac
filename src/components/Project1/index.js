0cd8d370fe19ea8a48fb3e7292d756a7e667d183
avc0021 <avc0021@gmail.com>
2022-03-22 23:13:57 -0500

    images and working links added to projects

--- src/components/Project1/index.js
+++ src/components/Project1/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import mns from '../../assets/proj-img/moon.png'
 
 
 function Project1 () {
@@ -8,9 +9,13 @@ function Project1 () {
           <h1 className="portfolio">Portfolio</h1>
           <div className="mns">
             <h3 className="mns-1">My Night Sky</h3>
+            <img src={mns} className="pImg" style={{ width: "30%" }} alt="cover" />
             <p className="descr">An app that tells you what is in the stars above you based on your geolocation or city input.</p>
             <h3>
-            <a href="https://github.com/coevpen/my-night-sky" >View Project on GitHub</a>
+            <a href="https://github.com/coevpen/my-night-sky" >View Project on GitHub</a> <br></br>
+              <a href="https://github.com/coevpen/my-night-sky/deployments/activity_log?environment=github-pages">
+                Deployed Link
+              </a>
             </h3>
           </div>
       </section>
