7946b00d5d5617682b1b5037f99ac4b6e4baaae8
avc0021 <avc0021@gmail.com>
2022-03-23 13:47:52 -0500

    updated descriptions on projects

--- src/components/Project3/index.js
+++ src/components/Project3/index.js
@@ -9,7 +9,7 @@ function Project3 () {
           <div className="bt">
             <h3>Budget Tracker</h3>
             <img src={bt} className="pImg" style={{ width: "65%" }} alt="cover" />
-            <p className="descr">An app that tells you what is in the stars above you based on your geolocation or city input.</p>
+            <p className="descr">Budget Tracker allows the user to track withdrawals and deposits with or without data/internet connection. This will allow the user to accurately balance account.</p>
             <h3>
             <a href="https://github.com/avc0021/Budget-Tracker">View Project on GitHub</a><br></br>
             <a href="https://stormy-headland-24832.herokuapp.com/">Deployed Link</a>
