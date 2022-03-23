e20b28f9d912a538073061b666c1304389b4cb06
avc0021 <avc0021@gmail.com>
2022-03-23 16:05:55 -0500

    updated paths for app.js

--- src/App.js
+++ src/App.js
@@ -14,21 +14,11 @@ function App() {
         <Header/> 
         <div className='content'>
           <Switch>
-          <Route exact path='/'>
-              <About/>
-            </Route>
-            <Route path='/about'>
-              <About/>
-            </Route>
-            <Route path='/projects'>
-              <Projects/>
-            </Route>
-            <Route path='/resume'>
-              <Resume/>
-            </Route> 
-            <Route path='/contact'>
-              <Contact/>
-            </Route> 
+            <Route exact path='/' component={About}/>
+            <Route exact path='/about' component={About}/>
+            <Route exact path='/projects' component={Projects}/>
+            <Route exact path='/resume' component={Resume}/>
+            <Route exact path='/contact' component={Contact}/>
           </Switch>
         </div>
         <Footer/>
