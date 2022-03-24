f51f04dd64f7706411848fcc2afad9e8ddd19acb
avc0021 <avc0021@gmail.com>
2022-03-23 21:35:54 -0500

    debug2

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import './index.css';
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
-<BrowserRouter basename='/portfolio-ac'>
-<App/>
-</BrowserRouter>, document.getElementById('root'));
+    <HashRouter basename={process.env.PUBLIC_URL} hashType="slash">
+      <App/>
+    </HashRouter>, document.getElementById('root'));
   </React.StrictMode>,
   document.getElementById('root')
 );
