06928e9de9f7c18efb5a6b21d14da7e8f701c91f
avc0021 <avc0021@gmail.com>
2022-03-24 22:33:57 -0500

    attempt to deploy to heroku

--- src/router/Router.js
+++ src/router/Router.js
@@ -11,7 +11,7 @@ export default class Router extends Component {
         return (
             <div>
                 <Switch>
-                    <Route path="/" exact component={About}/>
+                    <Route path="/portfolio-ac" exact component={About}/>
                     <Route path="/about" exact component={About}/>
                     <Route path="/projects" exact component={Projects}/>
                     <Route path="/resume" exact component={Resume}/>
