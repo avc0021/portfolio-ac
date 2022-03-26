9b98c703c3197f07be5add8e89980afc20f96375
avc0021 <avc0021@gmail.com>
2022-03-25 20:07:33 -0500

    Updates

---
+++ precache-manifest.5db6174295fda40b94c5aa591c943fb3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c0c2fbe51bc56390ee3",
+    "url": "/portfolio-ac/static/css/main.81b4ec5f.chunk.css"
+  },
+  {
+    "revision": "3c0c2fbe51bc56390ee3",
+    "url": "/portfolio-ac/static/js/main.3c0c2fbe.chunk.js"
+  },
+  {
+    "revision": "7d8a5b69bced8be18895",
+    "url": "/portfolio-ac/static/js/1.7d8a5b69.chunk.js"
+  },
+  {
+    "revision": "cc0d8b30c88f84d9da35",
+    "url": "/portfolio-ac/static/js/2.cc0d8b30.chunk.js"
+  },
+  {
+    "revision": "bfe2d4c9f65c7e05d967",
+    "url": "/portfolio-ac/static/js/runtime~main.bfe2d4c9.js"
+  },
+  {
+    "revision": "f3f12256e999da5c894d9528bdfc9fce",
+    "url": "/portfolio-ac/static/media/aj.f3f12256.jpg"
+  },
+  {
+    "revision": "b447d5e5886e96e6a3470865effabd5a",
+    "url": "/portfolio-ac/static/media/moon.b447d5e5.png"
+  },
+  {
+    "revision": "45e3ec7059f0d69344a896f201e9c70a",
+    "url": "/portfolio-ac/static/media/social.45e3ec70.png"
+  },
+  {
+    "revision": "b65ff9fca9a9cd6f6439cdba5776f41a",
+    "url": "/portfolio-ac/static/media/budget.b65ff9fc.png"
+  },
+  {
+    "revision": "703dd08ddf91953c9dd6717530720f4d",
+    "url": "/portfolio-ac/static/media/iru.703dd08d.png"
+  },
+  {
+    "revision": "fd71be5ec67dab865249f8c001111d38",
+    "url": "/portfolio-ac/static/media/mtn2.fd71be5e.jpg"
+  },
+  {
+    "revision": "56166e2322a90fb3131ce13072fc93ea",
+    "url": "/portfolio-ac/index.html"
+  }
+];
