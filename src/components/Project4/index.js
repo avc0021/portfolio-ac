08532994ba087f42060cb46b0c27ade53015bbc5
avc0021 <avc0021@gmail.com>
2022-03-28 21:46:58 -0500

    fixed alignment issues in projects 3 and 4

--- src/components/Project4/index.js
+++ src/components/Project4/index.js
@@ -5,12 +5,12 @@ import iru from '../../assets/proj-img/iru.png';
 function Project4 () {
 
     return (
-      <section className="projects-iru">
+      <section className="projects">
 
           <div className="iru">
             <h3 className="iru-1">Instruments R Us</h3>
-            <img src={iru} className="pImg" style={{ width: "80%" }} alt="cover" />
-            <p className="descr">Instruments R Us allows the user to post music instruments to share and sell.</p>
+            <img src={iru} className="pImg" style={{ width: "50%" }} alt="cover" />
+            <p className="descr">This app allows the user to post music instruments to share and sell.</p>
             <h3>
             <a href="https://github.com/avc0021/InstrumentsRUs">View Project on GitHub</a><br></br>
             <a href="https://mysterious-harbor-28313.herokuapp.com/">Deployed Link</a>
